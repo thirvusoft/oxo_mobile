da97cbbbe31583b93515907f5aa597a2b8b4efc0
vignesh235 <vickyjai003@gmail.com>
2023-04-27 13:42:26 +0530

    add marker icon for map

--- lib/screens/Location Map page/locationpin.dart
+++ lib/screens/Location Map page/locationpin.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:oxo/constants.dart';
+import 'dart:typed_data';
+import 'package:image/image.dart' as IMG;
+import 'dart:ui' as ui;
 
 class location_pin extends StatefulWidget {
   const location_pin({super.key});
@@ -25,6 +30,21 @@ class _location_pinState extends State<location_pin> {
   Completer<GoogleMapController> _controller = Completer();
   MapType _currentMapType = MapType.normal;
   List<Marker> _markers = [];
+
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
+// Use await to wait for the future to resolve
+
+// Add the marker with the custom icon to the map
+
   void _onMapTypeButtonPressed() {
     setState(() {
       _currentMapType = _currentMapType == MapType.normal
@@ -68,8 +88,9 @@ class _location_pinState extends State<location_pin> {
   Future location_list() async {
     var response = await http.get(Uri.parse(
         """${dotenv.env['API_URL']}/api/method/oxo.custom.api.location_list"""));
-    print(response.statusCode);
-    print(response.body);
+
+    final Uint8List markerIcon = await getBytesFromAsset("assets/Pin.png", 300);
+    // final Marker marker = Marker(icon: BitmapDescriptor.fromBytes(markerIcon));
     if (response.statusCode == 200) {
       setState(() {
         status = false;
@@ -81,16 +102,15 @@ class _location_pinState extends State<location_pin> {
         for (int j = 0; j <= location.length; j++) {
           setState(() {
             _markers.add(Marker(
-              markerId: MarkerId(location[j]["name"].toString()),
-              position:
-                  LatLng(location[j]["latitude"], location[j]["longitude"]),
-              onTap: () {},
-              infoWindow: InfoWindow(
-                title: location[j]["name"],
-                snippet: location[j]["pincode"],
-              ),
-              icon: BitmapDescriptor.defaultMarker,
-            ));
+                markerId: MarkerId(location[j]["name"].toString()),
+                position:
+                    LatLng(location[j]["latitude"], location[j]["longitude"]),
+                onTap: () {},
+                infoWindow: InfoWindow(
+                  title: location[j]["name"],
+                  snippet: location[j]["pincode"],
+                ),
+                icon: BitmapDescriptor.fromBytes(markerIcon)));
           });
         }
       });
